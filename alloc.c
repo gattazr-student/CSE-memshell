90ab4bc665dee93443f19aaf4b16d569a32e3709
WeHaveCookie <quentin.gerry@gmail.com>
2015-11-06 16:21:01 +0100

    first version of mem_alloc()

--- alloc.c
+++ alloc.c
@@ -7,13 +7,49 @@ void mem_init(){
     /* TODO: write function */
 }
 
+
+/*
+
+*/
 /**
  * mem_alloc
  * @param size
  */
 void *mem_alloc(size_t size){
     /* TODO: write function */
-    return NULL;
+    FreeBlock* currentPtr = mem_heap;
+    FreeBlock* oldPtr = mem_heap;
+    if (size < sizeof(FreeBlock))
+    {
+      size = sizeof(FreeBlock);
+    }
+    while (currentPtr->size < size) {
+      if(currentPtr->next == NULL) //Pas de zone memoire libre. On renvoie NULL
+      {
+        return NULL;
+      } else
+      { // On garde en mémoire le block courant et l'ancien.
+        oldPtr = currentPtr;
+        currentPtr = currentPtr->next;
+      }
+    }
+
+    //On a trouve un espace memoire disponible et assez grand.
+    // L'espace mémoire disponible est plus grand que requis.
+    fb* returnPtr = currentPtr;
+    if (returnPtr->size == size)
+    {
+      oldPtr->next = currentPtr->next;
+    } else
+    {
+      currentPtr = returnPtr+size;
+      currentPtr->size = returnPtr->size - size;
+      currentPtr->next = returnPtr->next;
+      oldPtr->next = currentPtr;
+    }
+
+    // On renvoie l'adresse de ptr
+    return returnPtr;
 }
 
 /**
