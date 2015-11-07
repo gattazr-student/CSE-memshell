37f8fd73714e3b80244e693d41514a21a07c36c4
Rémi GATTAZ <remi.gattaz@gmail.com>
2015-11-07 14:54:38 +0100

    FIX #2 : Use a global variable to point to the first FreeBlock instead of a FreeBlock in the heap

--- alloc.c
+++ alloc.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include "alloc.h"
 
+/* Pointeur sur le premier block free */
+/* Vaut NULL si il n'y en a pas */
+FreeBlock* first_FB = NULL;
+
 /**
  * mem_init
  * Initialise la liste des zones libres avec une seule zone correspondant à l'ensemble du tableau
@@ -11,20 +15,10 @@
  * effectuées, l’ensemble des structures de données est réinitialisé.
  */
 void mem_init(){
-    /* TODO: test function */
-    int wTailleStructFb;
-    /* Création de la liste gestion mémoire en tête du heap */
-    wTailleStructFb = sizeof(FreeBlock);
-
-    FreeBlock *wFirstBlock = (FreeBlock*) mem_heap;
-    FreeBlock *wFreeBlock = (FreeBlock*)((char *) mem_heap + wTailleStructFb);
-
-    wFirstBlock->size = 0;
-    wFirstBlock->next = wFreeBlock;
-
-    /* Création de l'élément vide en mémoire */
-    wFreeBlock->size = HEAP_SIZE - wTailleStructFb;
-    wFreeBlock->next = NULL;
+    /* Définit le heap complet comme une block libre */
+    first_FB = (FreeBlock*) mem_heap;
+    first_FB->size = HEAP_SIZE;
+    first_FB->next = NULL;
 }
 
 
@@ -33,43 +27,52 @@ void mem_init(){
 */
 /**
  * mem_alloc
- * @param size
+ * Retourne un pointeur sur une zone mémoire alloué de la taille ``size`` dans mem_heap. La taille
+ * est arrondie au plus petit multiple de la taille du descripteur de zone supérieur ou égal à
+ * size
+ * @param size : taille de la zone à allouer
+ * return : pointeur sur la zone alloué. NULL si allocation impossible
  */
 void *mem_alloc(size_t size){
-    /* TODO: write function */
-    FreeBlock* currentPtr = (FreeBlock*)mem_heap;
-    FreeBlock* oldPtr = (FreeBlock*)mem_heap;
-    if (size < sizeof(FreeBlock))
-    {
-      size = sizeof(FreeBlock);
-    }
-    while (currentPtr->size < size) {
-      if(currentPtr->next == NULL) //Pas de zone memoire libre. On renvoie NULL
-      {
-        return NULL;
-      } else
-      { // On garde en mémoire le block courant et l'ancien.
-        oldPtr = currentPtr;
-        currentPtr = currentPtr->next;
-      }
+    FreeBlock *wCurrentFB = first_FB;
+    /* Pointeur de pointeur du FreeBlock précédent */
+    /* Ce pointeur est utilisé pour remplacer la valeur next du FreeBLock précédent */
+    FreeBlock **wOldFBNext = &first_FB;
+
+    /* Calcul de la vrai taille du bloc à allouer */
+    int wFreeBlockSize = sizeof(FreeBlock);
+    int wDiff = size % wFreeBlockSize;
+    if (wDiff != 0){
+        size += wFreeBlockSize - wDiff;
     }
 
-    //On a trouve un espace memoire disponible et assez grand.
-    // L'espace mémoire disponible est plus grand que requis.
-    FreeBlock* returnPtr = currentPtr;
-    if (returnPtr->size == size)
-    {
-      oldPtr->next = currentPtr->next;
-    } else
-    {
-      currentPtr = (FreeBlock*) ((char*)returnPtr + size);
-      currentPtr->size = returnPtr->size - size;
-      currentPtr->next = returnPtr->next;
-      oldPtr->next = currentPtr;
+    /* Avance dans la liste des FreeBlock pour trouver un bloc sufisamment grand */
+    while(wCurrentFB->size < size){
+        /* La fin de la liste a été atteinte */
+        if(wCurrentFB->next == NULL){
+            return NULL;
+        }
+        /* Sauvegarde l'adresse du pointeur next de courant puis avance dans la liste */
+        else{
+            wOldFBNext = &(wCurrentFB->next);
+            wCurrentFB = wCurrentFB->next;
+        }
     }
 
-    // On renvoie l'adresse de ptr
-    return returnPtr;
+    /* wCurrentFB pointe vers un FreeBlock de taille sufissante */
+    /* Le block fait exactement la taille nécessaire */
+    if(wCurrentFB->size == size){
+        *wOldFBNext = wCurrentFB->next;
+    }
+    /* Le bloc est plus grand que necessaire */
+    else{
+        FreeBlock *wNewFreeBlock = (FreeBlock*) ((char*)wCurrentFB + size);
+        wNewFreeBlock->size = wCurrentFB->size - size;
+        wNewFreeBlock->next = wCurrentFB->next;
+        *wOldFBNext = wNewFreeBlock;
+    }
+
+    return wCurrentFB;
 }
 
 /**
@@ -82,13 +85,13 @@ void mem_free(void *zone, size_t size){
 }
 
 /**
- * mem_free
- * Utilise la fonction passé en paramètre pour afficher les spécifications de toutes les zones libres du heap.
+ * mem_show
+ * Utilise la fonction passé en paramètre pour afficher les spécifications de toutes les zones *
+ * libres du heap.
  * @param print : procedure affichant le bloc mémoire à une adresse et taille passé en paramètre
  */
 void mem_show(void (*print)(void *zone, size_t size)){
-    /* TODO: write function */
-    FreeBlock *wCurrentBlock = (FreeBlock*) mem_heap;
+    FreeBlock *wCurrentBlock = first_FB;
     int wI = 0;
     while(wCurrentBlock != NULL){
         printf("%d : ", wI);
